bbb36a69d3c8c03ced0e8544fbbfa4bd2487abb0
danielNevin <106940037+danielNevin@users.noreply.github.com>
2023-09-09 12:55:04 +1000

    Contact Panel Updates

--- portfolio/src/Components/ContactPanel/ResumeCard.js
+++ portfolio/src/Components/ContactPanel/ResumeCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ResumePDF from '../../Resources/Resume/Daniel Nevin Resume September 2023.pdf'
+import ResumePDF from '../../Resources/Resume/Daniel Nevin CV.pdf'
 
 export default function ResumeCard() {
   return(
@@ -12,24 +12,24 @@ export default function ResumeCard() {
       </p>
       <a href={ResumePDF} download='Daniel Nevin Resume' target="_blank">
         <button className="flex h-14 w-60 p-2 bg-gradient-to-br from-accent to-sky-500 items-center justify-center text-background font-bold tracking-wide text-xl lg:hover:from-sky-500 lg:hover:to-primary lg:hover:text-2xl lg:hover:shadow-lg lg:transition-all px-6 rounded-lg">
-          VIEW RESUME
+          DOWNLOAD CV
         </button>
       </a>
       <div>
         <p className="text-center">
           You can contact me at
         </p>
-        <button className="text-primary font-bold lg:hover:brightness-75" onClick={() => window.location = 'mailto:dnevin234@gmail.com'}>dnevin234@gmail.com</button>
+        <button className="text-primary font-bold lg:hover:brightness-75 lg:hover:scale-110 transition-all" onClick={() => window.location = 'mailto:dnevin234@gmail.com'}>dnevin234@gmail.com</button>
       </div>
       <div className="flex flex-col items-center justify-center gap-8">
         <p className="text-center">
           Or you can find me on Github and Linkedin
         </p>
         <div className="flex items-center justify-center gap-8">
-          <a href="https://github.com/danielNevin">
+          <a className="lg:hover:scale-110 transition-all" href="https://github.com/danielNevin">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png" alt="Github logo" className="h-10" />
           </a>
-          <a href="https://www.linkedin.com/in/daniel-nevin/">
+          <a className="lg:hover:scale-110 transition-all" href="https://www.linkedin.com/in/daniel-nevin/">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png" alt="Linkedin logo" className="h-10" />
           </a>
         </div>
